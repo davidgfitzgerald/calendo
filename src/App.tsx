1346e0ad3d599d5791a02d31eff2e2ee4f87e876
Dave Fitzgerald <david.fitzgerald@blackcowtech.uk>
2023-02-24 23:40:59 +0000

    #9 Add moveItem function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+/**
+ * Mutate `arr` by moving the item at `startIndex` to `endIndex`.
+ * 
+ * @param startIndex - The index of the item to move
+ * @param endIndex - The index to drop the item
+ * @param arr - The array to mutate
+ */
+function moveItem(startIndex: number, endIndex: number, arr: Array<any>) {
+    const originalElement = arr.splice(startIndex, 1)[0];
+    arr.splice(endIndex, 0, originalElement);
+}
+
+
 interface ItemsProps {
     items: Array<String>
 }
