0ad4ad2a924350c93af73e0b9161c8a20b133e76
Dave Fitzgerald <david.fitzgerald@blackcowtech.uk>
2023-02-25 18:51:52 +0000

    #9 Add event listener for onDragEnd

--- src/components/DragNDrop/DragNDrop.tsx
+++ src/components/DragNDrop/DragNDrop.tsx
@@ -7,21 +7,45 @@ function DragNDrop({ data }: DragNDropProps) {
     const [items, setItems] = useState(data);
     const [dragging, setDragging] = useState(false);
     const dragItem = useRef<ItemCoords>();
+    const dragNode = useRef<EventTarget>();
 
-    function onDragStart(event: React.DragEvent, params: ItemCoords) {
-        console.log(`Drag started on (${params.groupIndex}, ${params.itemIndex})`)
-        dragItem.current = params
-        setDragging(true)
+    const isDragItem = (params: ItemCoords): boolean => {
+        return dragItem.current !== undefined && dragItem.current.groupIndex == params.groupIndex && dragItem.current.itemIndex == params.itemIndex
     }
 
-    function setClassName(params: ItemCoords) {
-        let className = "dnd-item"
-        const current = dragItem.current
-        const isDragItem = (): boolean => {
-            return current !== undefined && current.groupIndex == params.groupIndex && current.itemIndex == params.itemIndex
-        }
+    function onDragStart(event: React.DragEvent, item: ItemCoords) {
+        console.log(`Drag started on (${item.groupIndex}, ${item.itemIndex})`)
+        dragItem.current = item
+        dragNode.current = event.target
+        dragNode.current.addEventListener("dragend", onDragEnd)
+        setTimeout(() => {
+            // Using setTimeout means the re-render is after  
+            // the drag start.This means the drag ghost is 
+            // the current item and not the blanked out item.
+            setDragging(true)
+        }, 0)
+    }
 
-        if (dragging && isDragItem()) {
+    function onDragEnd(event: Event): void {
+        if (dragItem.current === undefined) {
+            throw Error("Ending drag but dragItem is undefined")
+        }
+        if (dragNode.current === undefined) {
+            throw Error("Ending drag but no dragNode is defined")
+        }
+        
+        console.log("Ending drag")
+        dragNode.current.removeEventListener("dragend", onDragEnd)
+        dragItem.current = undefined;
+        dragNode.current = undefined;
+        setDragging(false)
+    }
+
+    function setClassName(item: ItemCoords) {
+        let className = "dnd-item"
+
+
+        if (dragging && isDragItem(item)) {
             className += " dragging"
         }
 
