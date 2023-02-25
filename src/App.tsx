b334ce79f17b8e96af0ea7468cdee8ad9361b4c8
Dave Fitzgerald <david.fitzgerald@blackcowtech.uk>
2023-02-25 12:12:40 +0000

    #9 Get cypress test passing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid'
 
 /**
- * Mutate `arr` by moving the item at `startIndex` to `endIndex`.
+ * Create a new array by moving the item at `startIndex` to `endIndex`.
  * 
  * @param startIndex - The index of the item to move
  * @param endIndex - The index to drop the item
  * @param arr - The array to mutate
+ * @returns Thew new items
  */
 function moveItem(startIndex: number, endIndex: number, arr: Array<any>) {
-    const originalElement = arr.splice(startIndex, 1)[0];
-    arr.splice(endIndex, 0, originalElement);
+    const originalElement = arr.splice(startIndex, 1)[0]
+    arr.splice(endIndex, 0, originalElement)
+    return [...arr]
 }
 
 
@@ -34,56 +36,49 @@ export function App(props: ItemListProps) {
 
 
 export function ItemList(props: ItemListProps) {
-    const style = {
-        listStyle: "none"
-    }
 
     const [items, setItems] = useState(props.items)
 
     function onDragStart(event: React.DragEvent<HTMLLIElement>, index: number) {
-        console.log(`Called onDragStart on item ${index}`)
-        event.dataTransfer.setData("startIndex", index.toString())
-    }
-    
+            console.log(`Called onDragStart on item ${index}`)
+            event.dataTransfer.setData("startIndex", index.toString())
+        }
+
     function onDragOver(event: React.DragEvent<HTMLLIElement>, index: number) {
-        console.log(`Called onDragOver on item ${index}`)
-        event.preventDefault();
-    }
-    
+            console.log(`Called onDragOver on item ${index}`)
+            event.preventDefault()
+        }
+
     function onDrop(event: React.DragEvent<HTMLLIElement>, endIndex: number) {
-        console.log(`Called onDrop on item ${endIndex}`)
-        const startIndex = parseInt(event.dataTransfer.getData("startIndex"))
-        event.preventDefault();
+            console.log(`Called onDrop on item ${endIndex}`)
+            const startIndex = parseInt(event.dataTransfer.getData("startIndex"))
+            event.preventDefault()
 
-        // Update the state with the dropped element
-        console.log(`move item from ${startIndex} to ${endIndex}`)
-        console.log(`items was ${items}`)
-        moveItem(startIndex, endIndex, items);
-        setItems(items)
-        console.log(`items is now ${items}`)
+            // Update the state with the dropped element
+            console.log(`move item from ${startIndex} to ${endIndex}`)
+            console.log(`items was ${items}`)
+            const newItems = moveItem(startIndex, endIndex, items)
+            setItems(newItems)
+            console.log(`items is now ${items}`)
+        }
 
-
+    const style = {
+        listStyle: "none"
     }
-
-    // this.handleScroll = this.handleScroll.bind(this)
-    // this.onDragStart = this.onDragStart.bind(this)
-    // this.onDragOver = this.onDragOver.bind(this)
-    // this.onDrop = this.onDrop.bind(this)
-
     return (
         <ul style={style}>
             {props.items.map((name, index) => {
                 return <Item
-                name={name} 
-                key={uuidv4()}
-                index={index}
-                onDragStart={(event) => onDragStart.bind(null, event, index)}
-                onDragOver={(event) => onDragOver.bind(null, event, index)}
-                onDrop={(event) => onDrop.bind(null, event, index)}
+                    name={name}
+                    key={uuidv4()}
+                    index={index}
+                    onDragStart={onDragStart}
+                    onDragOver={onDragOver}
+                    onDrop={onDrop}
                 />
             })}
         </ul>
-    );
+    )
 }
 
 
@@ -91,9 +86,9 @@ interface ItemProps {
     name: String
     key: String
     index: Number
-    onDragStart: React.DragEventHandler<HTMLLIElement>
-    onDragOver: React.DragEventHandler<HTMLLIElement>
-    onDrop: React.DragEventHandler<HTMLLIElement>
+    onDragStart: Function
+    onDragOver: Function
+    onDrop: Function
 }
 
 export function Item(props: ItemProps) {
@@ -107,11 +102,14 @@ export function Item(props: ItemProps) {
     const divStyle = {
         padding: 10
     }
+
     return (
-        <li style={liStyle} draggable={true}
-        onDragStart={props.onDragStart}
-        onDragOver={props.onDragOver}
-        onDropCapture={props.onDrop}
+        <li style={liStyle} 
+            draggable={true}
+            className={""}
+            onDragStart={(event) => props.onDragStart(event, props.index)}
+            onDragOver={(event) => props.onDragOver(event, props.index)}
+            onDrop={(event) => props.onDrop(event, props.index)}
         >
             <div style={divStyle}>
                 {props.name}
--- todo.md
+++ todo.md
@@ -4,3 +4,11 @@
     - Here is the GH repo: https://github.com/trananhtuat/fullstack-kanban-app
     - Also same author has this repo which may be useful: https://github.com/trananhtuat/react-kanban-ui/tree/main/src that uses react-beautiful-dnd though
 - [ ] This video uses react-dnd: https://www.youtube.com/watch?v=aK2PD_REk7A
+
+25/02/23
+
+Watched this video [Drag And Drop With React Hooks From Scratch](https://www.youtube.com/watch?v=Q1PYQPK9TaM).
+- Has a good CSS walkthrough
+- Uses `<div className="drag-n-drop">` to hold multiple `<div className="dnd-group">` containers
+- Within the `"dnd-group"` live the `"dnd-items"`
+- Takes an arbitrary number of groups and then an arbitrary number of items
