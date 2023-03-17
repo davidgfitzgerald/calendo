5580c566363599ada3d2856d392acdbd1b67b7a6
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2023-03-17 00:36:31 +0000

    refactor: Rename drag and drop variables

--- src/components/DragNDrop/DragNDrop.tsx
+++ src/components/DragNDrop/DragNDrop.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { DragNDropProps } from "./types";
+import { DragNDropProps, Item, ItemId } from "./types";
 import './DragNDrop.css';
 
 function DragNDrop({ data }: DragNDropProps) {
-  const { items, columns, columnOrder } = data
 
   return (
 
     <div className="drag-n-drop">
       {
-        columnOrder.map((columnId) => {
-          const column = columns[columnId]
+        data.columnOrder.map((columnId) => {
+          const column = data.columns[columnId]
+          const items = column.itemIds.map((itemId) => data.items[itemId])
           return (
             <div className="dnd-group" key={columnId}>
               <div className="dnd-group-title">{column.title}</div>
-              {column.itemIds.map((itemId) => {
-                const item = items[itemId]
+              {items.map((item) => {
                 return <div className="dnd-item">{item.content}</div>
               })}
             </div>
