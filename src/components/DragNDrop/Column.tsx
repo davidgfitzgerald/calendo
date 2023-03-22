981f1a40f71f39d917a50b192452088dd6878089
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2023-03-22 23:47:34 +0000

    Add Droppable to Column

--- src/components/DragNDrop/Column.tsx
+++ src/components/DragNDrop/Column.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ColumnProps } from "./types";
 import './DragNDrop.css';
+import { Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import Task from './Task';
 
@@ -21,12 +22,20 @@ const TaskList = styled.div`
 export default class Column extends React.Component<ColumnProps> {
   render() {
     return (
-    <Container>
-      <Title>{this.props.column.title}</Title>
-      <TaskList>
-        {this.props.tasks.map(task => <Task key={task.id} task={task}/>)}
-      </TaskList>
-    </Container>
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <Droppable droppableId={this.props.column.id}>
+          {(provided) => (
+            <TaskList
+              ref={provided.innerRef}  // innerRef had a type error
+              {...provided.droppableProps}
+            >
+              {this.props.tasks.map(task => <Task key={task.id} task={task} />)}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
+      </Container>
     )
   }
 }
