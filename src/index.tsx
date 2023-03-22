85555aa8710f9d8ec0140d08effe709396269e12
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2023-03-22 23:17:59 +0000

    Add styled-components and normalize.css npm packages

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './colours.css'
 import { App } from './App';
+import 'normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
 
 root.render(
   <React.StrictMode>
