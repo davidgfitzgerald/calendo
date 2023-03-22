975977fad4e2793e89b3601dbdf982f9301fe822
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2023-03-22 23:47:14 +0000

    Add DragDropContext to DragNDrop

--- src/components/DragNDrop/DragNDrop.tsx
+++ src/components/DragNDrop/DragNDrop.tsx
@@ -2,19 +2,30 @@ import React from 'react';
 import { DragNDropProps } from "./types";
 import './DragNDrop.css';
 import Column from './Column';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 
 export class DragNDrop extends React.Component<DragNDropProps, {}> {
   state = this.props.data;
 
+  onDragEnd = result => {
+    // TODO re-order our column
+  }
+
   render() {
-      return this.state.columnOrder.map((columnId) => {
+    return (
+      <DragDropContext
+        onDragEnd={this.onDragEnd}
+      
+      >
+        {this.state.columnOrder.map((columnId) => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
-
           return (
             <Column key={columnId} column={column} tasks={tasks}></Column>
-            )
-      })
+          )
+        })}
+      </DragDropContext>
+    )
   }
 }
